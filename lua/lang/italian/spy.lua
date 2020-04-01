0e8b2731893d66c7ca25ea88e4bccb0947609093
Alessandro Levante <62677560+PinoMartirio@users.noreply.github.com>
2020-04-01 19:33:36 +0200

    Added Italian translations
    Added full Italian translations for the Spy role.

---
+++ lua/lang/italian/spy.lua
@@ -0,0 +1,15 @@
+L = LANG.GetLanguageTableReference("italian")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[SPY.name] = "Spia"
+L["info_popup_" .. SPY.name] = [[Sei una Spia! Una Spia gioca con gli innocenti ma viene mostrato come traditore per i traditori per confonderli.]]
+L["body_found_" .. SPY.abbr] = "Era una Spia!"
+L["search_role_" .. SPY.abbr] = "Questa persona era una Spia!"
+L["target_" .. SPY.name] = "Spia"
+L["ttt2_desc_" .. SPY.name] = [[Una Spia è un normale innocente che viene mostrato come traditore, il suo obiettivo è quello di scoprire i traditori e ucciderli.
+Può far finta di comprare da uno shop apposito.]]
+
+-- OTHER ROLE LANGUAGE STRINGS
+L["ttt2_teamchat_jammed_" .. SPY.name] = "Non puoi chattare con la tua squadra finché ci sono Spie in vita!"
+L["ttt2_teamvoice_jammed_" .. SPY.name] = "Non puoi usare la chat vocale della squadra finché ci sono Spie in vita!"
+L["ttt2_fakebuy_success_" .. SPY.name] = "Hai fatto finta di comprare un oggetto con successo!"
