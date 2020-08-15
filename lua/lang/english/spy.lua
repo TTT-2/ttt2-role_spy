e0780f2d933989ef97b60c7a899779eca1df620b
Aram <dragolino6@mail.ru>
2020-08-15 19:00:37 +0300

    Updated English localization file; Added Russian localization (#22)

--- lua/lang/english/spy.lua
+++ lua/lang/english/spy.lua
@@ -2,13 +2,13 @@ L = LANG.GetLanguageTableReference("english")
 
 -- GENERAL ROLE LANGUAGE STRINGS
 L[SPY.name] = "Spy"
-L["info_popup_" .. SPY.name] = [[You are a spy! A spy plays in the innocent team but is shown to the traitos as their mate to confuse them.]]
-L["body_found_" .. SPY.abbr] = "They were a spy!"
-L["search_role_" .. SPY.abbr] = "This person was a spy!"
+L["info_popup_" .. SPY.name] = [[You are a Spy! A Spy plays in the innocent team but is shown to the traitors as their mate to confuse them.]]
+L["body_found_" .. SPY.abbr] = "They were a Spy!"
+L["search_role_" .. SPY.abbr] = "This person was a Spy!"
 L["target_" .. SPY.name] = "Spy"
 L["ttt2_desc_" .. SPY.name] = [[]]
 
 -- OTHER ROLE LANGUAGE STRINGS
-L["ttt2_teamchat_jammed_" .. SPY.name] = "You are not able to chat with your team until every Spy is dead!"
-L["ttt2_teamvoice_jammed_" .. SPY.name] = "You are not able to use the team voice chat until every Spy is dead!"
-L["ttt2_fakebuy_success_" .. SPY.name] = "You successfully faked an equipment purchase!"
+L["ttt2_teamchat_jammed_" .. SPY.name] = "You cannot use the team text chat until every Spy is dead!"
+L["ttt2_teamvoice_jammed_" .. SPY.name] = "You cannot use the team voice chat until every Spy is dead!"
+L["ttt2_fakebuy_success_" .. SPY.name] = "You successfully made a fake equipment purchase!"
