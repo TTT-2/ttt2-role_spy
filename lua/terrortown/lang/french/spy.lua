ea1bf0efae0613be3628f578b150cf884187589b
MisterClems <73431903+MisterClems@users.noreply.github.com>
2020-10-30 00:52:55 +0100

    Create spy.lua
    French translation :Spy

---
+++ lua/terrortown/lang/french/spy.lua
@@ -0,0 +1,14 @@
+local L = LANG.GetLanguageTableReference("french")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[SPY.name] = "Espion"
+L["info_popup_" .. SPY.name] = [[Vous êtes un Espion! Un Espion joue dans l'équipe des innocents mais est présenté aux traîtres comme leur compagnon pour les embrouiller.]]
+L["body_found_" .. SPY.abbr] = "C'était un Espion!"
+L["search_role_" .. SPY.abbr] = "Cette personne était un Espion!"
+L["target_" .. SPY.name] = "Espion"
+L["ttt2_desc_" .. SPY.name] = [[]]
+
+-- OTHER ROLE LANGUAGE STRINGS
+L["ttt2_teamchat_jammed_" .. SPY.name] = "Vous ne pouvez pas utiliser le chat de votre équipe tant que tous les Espions ne sont pas morts!"
+L["ttt2_teamvoice_jammed_" .. SPY.name] = "Vous ne pouvez pas utiliser le chat vocal de votre équipe tant que tous les Espions ne sont pas morts!"
+L["ttt2_fakebuy_success_" .. SPY.name] = "Vous avez réussi à faire un faux achat d'équipement!"
