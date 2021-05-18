92fab7913440b40456979554985bf0f013068045
Tim Goll <tim@timgoll.de>
2021-05-18 08:54:47 +0200

    support for multiple spies in a round; cleanup

--- lua/terrortown/events/spy_alive.lua
+++ lua/terrortown/events/spy_alive.lua
@@ -63,11 +63,17 @@ hook.Add("TTT2AddedEvent", "trigger_spy_survival_event", function(type)
 	if type ~= EVENT_FINISH then return end
 
 	local plys = util.GetAlivePlayers()
+	local spyAlive = false
+
 	for i = 1, #plys do
-		if plys[1]:GetSubRole() == ROLE_SPY then
-			events.Trigger(EVENT_SPY_ALIVE)
-			return
-		end
+		if plys[1]:GetSubRole() ~= ROLE_SPY then continue end
+
+		spyAlive = true
+
+		break
 	end
 
+	if not spyAlive then return end
+
+	events.Trigger(EVENT_SPY_ALIVE)
 end)
